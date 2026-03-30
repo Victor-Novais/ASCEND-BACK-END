42458bf0d13a16ae9225a21527192bb2cd93a40b
Victor-Novais <victornovais1337@gmail.com>
2026-03-30 15:56:19 -0300

    feat(prisma): integrando o Prisma com o NestJS usando o PrismaService e o módulo.

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log:
+        process.env.NODE_ENV === 'development'
+          ? ['query', 'info', 'warn', 'error']
+          : ['warn', 'error'],
+    });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication): Promise<void> {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
